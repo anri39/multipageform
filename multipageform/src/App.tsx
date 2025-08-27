e1552db9c0b9632b3bff58335be27c803e23e5a2
anri <anriugrexelidze2006@gmail.com>
2025-08-27 14:49:22 +0400

    bugfix in app.tsx

--- multipageform/src/App.tsx
+++ multipageform/src/App.tsx
@@ -1,44 +1,29 @@
 import { useState } from "react";
+import "./App.css";
 import FirstPart from "./components/FirstPart";
+import SideBar from "./components/SideBar";
 import SecondPart from "./components/SecondPart";
-import ThirdPart from "./components/ThirdPart";
-
-type FormData = {
-  name: string;
-  email: string;
-  addons: {
-    extraStorage: boolean;
-    customProfile: boolean;
-    notifications: boolean;
-  };
-};
+import Thirdpart from "./components/ThirdPart";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState(0);
-
-  const [formData, setFormData] = useState<FormData>({
-    name: "",
-    email: "",
-    addons: {
-      extraStorage: false,
-      customProfile: false,
-      notifications: false,
-    },
-  });
-
+  const [currentPage, setCurrentPage] = useState<number>(0);
   const nextPage = () => setCurrentPage((prev) => Math.min(prev + 1, 3));
   const prevPage = () => setCurrentPage((prev) => Math.max(prev - 1, 0));
-
   return (
-    <div className="container">
-      {currentPage === 0 && <FirstPart handleNextStep={nextPage} />}
-      {currentPage === 1 && (
-        <SecondPart handleNextStep={nextPage} goBack={prevPage} />
-      )}
-      {currentPage === 2 && (
-        <ThirdPart handleNextStep={nextPage} goBack={prevPage} />
-      )}
-    </div>
+    <>
+      <div className="container">
+        <SideBar currentpage={currentPage} />
+        <div className="sidecontent">
+          {currentPage === 0 && <FirstPart handleNextStep={nextPage} />}
+          {currentPage === 1 && (
+            <SecondPart handleNextStep={nextPage} goBack={prevPage} />
+          )}
+          {currentPage === 2 && (
+            <Thirdpart handleNextStep={nextPage} goBack={prevPage} />
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
